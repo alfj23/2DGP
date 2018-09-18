8577dbfde47081992a3a9ee09736c75486739e13
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-09-18 17:55:48 +0900

    Drill05 사각형의 뼈대 완성
    사각형을 그리기 위해 이동하는 함수들의 틀을 짰음.

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -4,9 +4,22 @@ open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
-
+def move_from_center_to_right():
+    pass
+def move_up():
+    pass
+def move_left():
+    pass
+def move_down():
+    pass
+def move_left_to_center():
+    pass
 def make_rectangle():
-    pass # When You make empty block.
+     move_from_center_to_right()
+     move_up()
+     move_left()
+     move_down()
+     move_left_to_center()
 def make_circle():
     pass
 
