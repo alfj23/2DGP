6a02c505f229145655106d2bbfa378f7e2d15647
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-09-18 23:51:22 +0900

    REAL Drill05 완성
    2014182019 박태준
    https://www.youtube.com/watch?v=xMeuGEU4OfE&feature=youtu.be
    리스트 제거함(사유 : 미사용)
    커밋 횟수 : 15회

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -5,8 +5,6 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-Array_Coordinate = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136, 316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
-
 def Move_1to2():
     x1, y1 = 203, 535
     x2, y2 = 132, 243
