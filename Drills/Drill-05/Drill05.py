f639e1a15c7d7003f2c3b3633be479623e533900
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-09-18 21:29:40 +0900

    REAL Drill05 GCfromArray() Func Comp
    리스트로부터 현재 좌표 다음 좌표를
    슬라이스해오는 함수 완성
    
    현재 x1, y1 다음 x2, y2

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -6,16 +6,20 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 
-Array_Coordinate = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136, 316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
 
-def Get_Coord_from_Array():
-   
-    pass
-def Move(Coord_x, Coord_y):
+x1,x2 = 0, 0
+y1,y2 = 0, 0
+def Get_Coord_from_Array(i, j):
+    Array_Coordinate = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136,
+                        316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
+    x1, y1 = Array_Coordinate[i:j:]
+    x2, y2 = Array_Coordinate[i+2:j+2:]
+    return x1, y1, x2, y2
 
+def Move(x, y):
     pass
 while True:
-    Get_Coord_from_Array()
+    Get_Coord_from_Array(i, j)
     Move()
     pass
     
