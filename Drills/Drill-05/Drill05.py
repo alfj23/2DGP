cfee5423527791f5447b67213237edc7b17c79d1
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-09-18 18:10:15 +0900

    Drill05 moveup() Func Complete
    moveup() 함수 구현 완료
    Test Read Time 을 줄이기 위하여
    move_from_center_to_right() 커멘트 처리하여 검사함.

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -12,10 +12,15 @@ def move_from_center_to_right():
         character.draw_now(x, y)
         x += 2
         delay(0.01)
-
-
 def move_up():
-    pass
+    x, y = 800 - 25, 50 + 40  # 잔디 50
+    while y < 600 - 50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
+
 def move_left():
     pass
 def move_down():
