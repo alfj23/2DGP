4dec808b2e6aae0554334fc531886c36beda2def
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-09-18 23:10:57 +0900

    REAL Drill05 FrameWork
    함수 한개로 하는 것은 힘들어서 일일히
    함수정의해서 사용하는 방향으로 전환하였음.
    
    기본 알고리즘은 현재 좌표와 다음 좌표값을 알고 있으므로 기울기를 구하여
    y1 (현재 y좌표) 값을 변화시키는 것임

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -5,71 +5,28 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
+Array_Coordinate = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136, 316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
 
-
-x1,x2 = 0, 0
-y1,y2 = 0, 0
-def Get_Coord_from_Array(i, j):
-    Array_Coordinate = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136,
-                        316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
-    x1, y1 = Array_Coordinate[i:j:]
-
-    if(i == 18 & j == 19):
-        x2, y2 = Array_Coordinate[(i-18):(j-20):]
-    else:
-        x2, y2 = Array_Coordinate[(i + 2):(j + 2):]
-
-    return x1, y1, x2, y2
-def Swap(a, b):
-    tmp = a
-    a = b
-    b = tmp
-def Move():
-    x1, y1, x2, y2 = Get_Coord_from_Array(i, j)
+def Move_1():
+    x1, y1 = 203, 535
+    x2, y2 = 132, 243
     X_Gap = x2 - x1
     Y_Gap = y2 - y1
     Inclination = Y_Gap / X_Gap # 기울기
 
     frame = 0
-    if (x1 > x2): # 현재 x 값이 다음 x 값 보다 클때
-       # Swap(x1, x2)
-        while x1 > x2: #방향 왼쪽
-            clear_canvas_now()
-            grass.draw(400, 30)
-            character.clip_draw(frame * 0, 0, 100, 100, x1, y1)
-            update_canvas()
-            frame = (frame + 1) % 8
-            x1 -= 3
-            if(y1 < y2):
-                y1 += 3 * Inclination
-            else:
-                y1 -= 3 * Inclination
-            delay(0.01)
-            get_events()
+    while x1 > x2:
+        clear_canvas_now()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 -= 3
+        y1 -= 3 * Inclination
+        delay(0.01)
+        get_events()
 
-    else:
-        while x1 < x2:
-            clear_canvas_now()
-            grass.draw(400, 30)
-            character.clip_draw(frame * 100, 0, 100, 100, x1, y1)
-            update_canvas()
-            frame = (frame + 1) % 8
-            x1 += 3
-            if (y1 < y2):
-                y1 += 3 * Inclination
-            else:
-                y1 -= 3 * Inclination
-            delay(0.01)
-            get_events()
-i = 0
-j = i + 2
 while True:
-    Get_Coord_from_Array(i, j)
-    Move()
-    i += 2
-    j += 2
-    if(i == 18 & i == 20):
-        i = 0
-        j = i + 2
+    pass
     
 close_canvas()
