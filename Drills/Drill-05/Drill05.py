11a53d77e4d240a0a6f78baf5290f7fe355a640d
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-09-18 18:14:05 +0900

    Drill05 Move_left() Func Complete
    Move_left() Function Development
    complete!

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -22,14 +22,20 @@ def move_up():
         delay(0.01)
 
 def move_left():
-    pass
+    x, y = 800 - 25, 600 - 50
+    while x > 0 + 25:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x -= 2
+        delay(0.01)
 def move_down():
     pass
 def move_left_to_center():
     pass
 def make_rectangle():
-     move_from_center_to_right()
-     move_up()
+     # move_from_center_to_right()
+     # move_up()
      move_left()
      move_down()
      move_left_to_center()
