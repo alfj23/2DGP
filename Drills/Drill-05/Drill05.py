a7ff9311598be5711c55afb1e8de353d35f6afe1
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-09-18 18:52:55 +0900

    REAL Drill05 Making FrameWork
    05번 과제 뼈대 개발
    Move(x, y) 함수는 10개 좌표가 입력되있는 리스트에서 좌표를 받게하려고 함.

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -3,7 +3,16 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
+
+Array_Coordinate = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136, 316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
+
+
+def Move(x, y):
+    pass
+while True:
+    Move()
+    pass
     
 close_canvas()
