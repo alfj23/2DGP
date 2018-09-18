a0541ceb1bf93f6147e27219ad6b2b9118bf3f7a
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-09-18 19:03:12 +0900

    REAL Drill05 FrameWork
    리스트에서 좌표를 가져올 Get_Coord_from_Array() 함수 추가

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -8,10 +8,14 @@ character = load_image('animation_sheet.png')
 
 Array_Coordinate = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136, 316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
 
+def Get_Coord_from_Array():
+   
+    pass
+def Move(Coord_x, Coord_y):
 
-def Move(x, y):
     pass
 while True:
+    Get_Coord_from_Array()
     Move()
     pass
     
