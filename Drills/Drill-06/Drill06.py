dc685ba8c4e6d2e3a5f13d8411fd0f89f4bb935a
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-09-20 18:59:27 +0900

    Drill06 개발중
    좌표값을 커서좌표값, 현재캐릭터 좌표값
    다음 캐릭터 좌표값 세개로 주었음

--- Drills/Drill-06/Drill06.py
+++ Drills/Drill-06/Drill06.py
@@ -6,7 +6,7 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
     global running
     global x, y
-    global Char_x, Char_y
+    global Char_x1, Char_y1 , Char_x2, Char_y2
 
     events = get_events()
     for event in events:
@@ -14,9 +14,10 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            Char_x, Char_y = event.x - 25, KPU_HEIGHT - 1 - event.y + 25
+            Char_x2, Char_y2 = event.x - 25, KPU_HEIGHT - 1 - event.y + 25
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -26,21 +27,35 @@ hand_arrow = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-Char_x, Char_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+Char_x1, Char_y1 = KPU_WIDTH // 3, KPU_HEIGHT // 3
+Char_x2, Char_y2 = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
-
+x_change = (Char_x1 - Char_x2)
+y_change = (Char_y1 - Char_y2)
+m = y_change / x_change
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     hand_arrow.draw(x, y)
-    # x_change = (Char_x - x)
-    # y_change = (Char_y - y)
-    # m = y_change / x_change
-    if x < Char_x:  # 우측을 바라봄.
-        character.clip_draw(frame * 100, 0 * 1, 100, 100, Char_x, Char_y)
-    elif x > Char_x:  # 좌측을 바라봄.
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, Char_x, Char_y)
+
+    if Char_x1 < Char_x2:  # 우측을 바라봄.
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, Char_x1, Char_y1)
+        Char_x1 += 2
+        if(0<= m <= 1):
+            Char_y1+=m
+        elif m > 1:
+            Char_x1 -= 1 / m
+            Char_y1 += 1
+    elif Char_x1 > Char_x2:  # 좌측을 바라봄.
+        character.clip_draw(frame * 100, 0 * 1, 100, 100, Char_x1, Char_y1)
+        if(0<= m <= 1):
+            Char_y1 -= m
+            Char_x1 -= 2
+        elif m > 1:
+            Char_x1 -= 1/m
+            Char_y1 += 1
+
     update_canvas()
     frame = (frame + 1) % 8
 
@@ -48,3 +63,7 @@ while running:
     handle_events()
 
 close_canvas()
+
+
+
+
