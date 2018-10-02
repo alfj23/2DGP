eaee484a8f4b2416245dc6c2ede9a766399fe83b
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-10-02 18:56:51 +0900

    Drill 07 리스트 컴프리핸션 완성
    랜덤한 20개의 좌표를 원소로 가지는 리스트 완성 (리스트 컴프리핸션 이용)

--- Drills/Drill-07/Drill07.py
+++ Drills/Drill-07/Drill07.py
@@ -10,6 +10,11 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 def Move(p1, p2):
     pass
 
-Move()
+
+Random_Coordinates = [(random.randint(0+25, 1280-25), random.randin(-350, 350)) for i in range(20)]
+i = 1
+while True:
+    Move(Random_Coordinates[n-1], Random_Coordinates[n])
+    n = (n + 1) & 20
 
 close_canvas()
