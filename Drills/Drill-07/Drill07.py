1d3575a4e0f1268b8b65d77f9d495203b4f16dbd
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-10-02 18:51:27 +0900

    Drill 07 시작
    Parametric representation, list comprehension을 이용예정

---
+++ Drills/Drill-07/Drill07.py
@@ -0,0 +1,15 @@
+from pico2d import *
+import random
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+Character = load_image('animation_sheet.png')
+Map = load_image('KPU_GROUND.png')
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+
+def Move(p1, p2):
+    pass
+
+Move()
+
+close_canvas()
