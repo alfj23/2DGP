0e2caf4ed0b1a074d4ee7ff410dad8d29adfe6af
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-10-04 18:57:42 +0900

    Drill08-2
    Move_character() 함수 파라미터를
    리스트 컴프리헨션으로 만든 리스트 값을 넣어야하는데, i+3 부분때문에 터짐
    
    캐릭터가 지나간 자리에 이미지가 남아있던 문제 해결.

--- Drills/Drill-08/Drill08.py
+++ Drills/Drill-08/Drill08.py
@@ -9,30 +9,13 @@ Map = load_image('KPU_GROUND.png')
 
 def Move_character(p1, p2, p3, p4):
     frame = 0
-    for i in range(0, 100 + 1, 20):
+    # draw p1-p2
+    for i in range(0, 50, 2):
         clear_canvas()
         Map.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-
-        # draw p1-p2
-        for i in range(0, 50, 2):
-            t = i / 100
-            x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
-            y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
-
-        # draw p2-p3
-        for i in range(0, 100, 2):
-            t = i / 100
-            x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
-                        -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
-            y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
-                        -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
-
-        # draw p3-p4
-        for i in range(50, 100, 2):
-            t = i / 100
-            x = (2 * t ** 2 - 3 * t + 1) * p2[0] + (-4 * t ** 2 + 4 * t) * p3[0] + (2 * t ** 2 - t) * p4[0]
-            y = (2 * t ** 2 - 3 * t + 1) * p2[1] + (-4 * t ** 2 + 4 * t) * p3[1] + (2 * t ** 2 - t) * p4[1]
-
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
         if (p1 < p2):  # See Right
             character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
         elif (p1 > p2):  # See Left
@@ -41,9 +24,42 @@ def Move_character(p1, p2, p3, p4):
         frame = (frame + 1) % 8
         delay(0.05)
 
-size = 20
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        clear_canvas()
+        Map.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+        if (p2 < p3):  # See Right
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        elif (p2 > p3):  # See Left
+            character.clip_draw(frame * 100, 0 * 1, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+    # draw p3-p4
+    for i in range(50, 100, 2):
+        clear_canvas()
+        Map.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p2[0] + (-4 * t ** 2 + 4 * t) * p3[0] + (2 * t ** 2 - t) * p4[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p2[1] + (-4 * t ** 2 + 4 * t) * p3[1] + (2 * t ** 2 - t) * p4[1]
+    if (p3 < p4):  # See Right
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    elif (p3 > p4):  # See Left
+        character.clip_draw(frame * 100, 0 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
+
+size = 10
 Random_Coordinates = [(random.randint(0+25, 800-25), random.randint(0+50, 600-50)) for i in range(size)]
 i = 1
 while True:
-    Move_character(Random_Coordinates[i-1], Random_Coordinates[i], Random_Coordinates[i+1],Random_Coordinates[i+2])
-    i = (i + 3) % size
+   # Move_character((200,200),(400,400),(600,200),(700,400))
+    Move_character(Random_Coordinates[i], Random_Coordinates[i+1], Random_Coordinates[i+2],Random_Coordinates[i+3])
+    i = (i + 1) % size
