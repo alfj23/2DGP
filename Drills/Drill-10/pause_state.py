ac26312ac1bb8b97d762ece7d8da9769587fa0bb
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-10-11 20:23:28 +0900

    Drill-10
    pause state 모듈 뼈대 완성

---
+++ Drills/Drill-10/pause_state.py
@@ -0,0 +1,14 @@
+import game_framework
+from pico2d import *
+
+import main_state
+
+__name__ = "PauseState"
+
+def enter(): pass
+def exit():  pass
+def update(): pass
+def draw(): pass
+def handle_events(): pass
+def pause(): pass
+def resume(): pass
