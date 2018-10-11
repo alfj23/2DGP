2d585d675904249694775f48cafe26964b078cee
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-10-11 21:04:28 +0900

    Drill-10 PS
    update 부분 필요없어서 지움.
    p를 다시 누르면 (메인 스테이트)이전 상태로 팝함.

--- Drills/Drill-10/pause_state.py
+++ Drills/Drill-10/pause_state.py
@@ -15,16 +15,25 @@ def exit():
     del(image)
 
 def update():
-    global pause_time
-    if pause_time % 2 == 1:
-        del(image)
-    pause_time += 0.01
+
     pass
 def draw():
-    
-    pass
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+
 def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
     pass
+
 def pause():
     pass
 def resume():
