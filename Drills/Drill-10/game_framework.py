0162b3cb0257270aa1306312d93552530c62c26a
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-10-11 20:14:19 +0900

    Drill-10
    pause 이미지 웹에서 가지고 옴.
    일지정지 상태로 전환 위한 pause_state.py 모듈 추가

--- Drills/Drill-10/game_framework.py
+++ Drills/Drill-10/game_framework.py
@@ -85,12 +85,15 @@ def quit():
 def run(start_state):
     global running, stack
     running = True
+
     stack = [start_state]
     start_state.enter()
+
     while (running):
-        stack[-1].handle_events()
+        stack[-1].handle_events() #
         stack[-1].update()
         stack[-1].draw()
+
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
         stack[-1].exit()
