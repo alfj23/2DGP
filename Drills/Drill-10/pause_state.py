a4ca330e4da5890b224c1d3080e5022c3d146bc9
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-10-11 21:06:29 +0900

    Drill-10 PS 모듈 완성
    필요없는 부분 삭제(메인스테이트 모듈 임포트 부분 등)

--- Drills/Drill-10/pause_state.py
+++ Drills/Drill-10/pause_state.py
@@ -1,27 +1,32 @@
 import game_framework
 from pico2d import *
 
-import main_state
-
 __name__ = "PauseState"
 image = None
 pause_time = 0.0
+
+
 def enter():
     global image
     image = load_image('pause.png')
 
+
 def exit():
     global image
-    del(image)
+    del image
+
 
 def update():
 
     pass
+
+
 def draw():
     global image
     clear_canvas()
     image.draw(400, 300)
     update_canvas()
+
 
 def handle_events():
     events = get_events()
@@ -32,9 +37,11 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
             game_framework.pop_state()
-    pass
+
 
 def pause():
     pass
+
+
 def resume():
     pass
