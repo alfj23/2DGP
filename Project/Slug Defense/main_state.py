2b92bfbdcd3a654be53a36a8357a62bdf556488d
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-10-17 14:04:20 +0900

    Project 플레이어 캐릭터 구현
    플레이어 캐릭터 구현을 위해 클래스 생성함.

---
+++ Project/Slug Defense/main_state.py
@@ -0,0 +1,44 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+name ="MainState"
+
+class player():
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    
+
+def enter():
+   pass
+
+
+def exit():
+   pass
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+       pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
+
+
+
