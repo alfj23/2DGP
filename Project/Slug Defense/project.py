95653a9f59daf267b14a7bb280b9155e8e11ee8a
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-10-18 23:02:39 +0900

    Project 캐릭터 구현
    아이들, 이동 스트라이트 시트 완성
    현재는 이동 이미지만 사용

--- Project/Slug Defense/project.py
+++ Project/Slug Defense/project.py
@@ -1,6 +1,9 @@
 from pico2d import *
 import random
 
+global running
+global dir
+global Current_direction
 
 class Grass:
     def __init__(self):
@@ -14,21 +17,45 @@ class Boy:
     def __init__(self):
         self.x, self.y = 0, 70
         self.frame = random.randint(0, 7)
-        self.image = load_image('SlugGunner.png')
+        self.image = load_image('Idle+moving.png')
 
     def update(self):
-        self.frame = (self.frame + 1) % 28
-        self.x += 5
+        if dir > 0:
+            self.frame = (self.frame + 1) % 18
+        if dir < 0:
+            self.frame = 18
+            self.frame = (self.frame -1) 
+        #self.x += 5
 
     def draw(self):
-        self.image.clip_draw(self.frame * 85, 420, 85, 85, self.x, self.y)
+        self.image.clip_draw(self.frame * 80, 0, 80-3, 80, self.x, self.y)
 
 def handle_events():
-    pass
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:  # 우측화살표 키
+                dir = 1
+                slug.x += 5 * dir
+            elif event.key == SDLK_LEFT:  # 좌측화살표 키
+                dir = -1
+                slug.x += 5 * dir
+            elif event.key == SDLK_ESCAPE:  # ESC 키
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir = -1
+                # Current_direction = 1
+            elif event.key == SDLK_LEFT:
+                dir = 1
+                # Current_direction = 0
 
 # initialization code
 
-
+running = True
 open_canvas()
 
 slug = Boy()
@@ -41,6 +68,7 @@ while True:
 
 
     clear_canvas()
+    get_events()
     slug.update()
     grass.draw()
     slug.draw()
