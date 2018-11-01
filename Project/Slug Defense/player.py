61270ab86bf632f85636af519e2703fc87c5c452
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-01 19:54:51 +0900

    Project 리팩토링
    플레이어 캐릭터 시간 적용 완료.
    공격 애니메이션 자연스럽게 출력되고,
    공격 후 이동 시 공격 애니메이션 지속 출력되던 문제 해결!

--- Project/Slug Defense/player.py
+++ Project/Slug Defense/player.py
@@ -1,3 +1,4 @@
+import game_framework
 from pico2d import *
 from cannon import Cannon
 
@@ -5,8 +6,8 @@ import game_world
 
 # Player Slug Drive Speed
 
-PIXEL_PER_METER = (10.0 / 0.3)
-RUN_SPEED_KMPH = 20.0
+PIXEL_PER_METER = (10.0 / 0.4)
+RUN_SPEED_KMPH = 60.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -15,10 +16,10 @@ RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 TIME_PER_ACTION = 0.5  # 액션 당 시간
 ACTION_PER_TIME = 1.0  # 액션 마다 달라서 따로 빼놓음?
-FRAMES_PER_ACTION = 8
+FRAMES_PER_ACTION = 23
 
 
-# Plater Slug Events
+# Player Slug Events
 RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, X = range(5)
 
 key_event_table = {
@@ -37,74 +38,78 @@ class IdleState:
     @staticmethod
     def enter(player, event):
         if event == RIGHT_DOWN:
-            player.velocity += 1
+            player.velocity += RUN_SPEED_PPS
         elif event == LEFT_DOWN:
-            player.velocity -= 1
+            player.velocity -= RUN_SPEED_PPS
         elif event == RIGHT_UP:
-            player.velocity -= 1
+            player.velocity -= RUN_SPEED_PPS
         elif event == LEFT_UP:
-            player.velocity += 1
+            player.velocity += RUN_SPEED_PPS
 
     @staticmethod
     def exit(player, event):
         if event == X:
-            player.check_cannon = True
-            player.fire_cannon()
-
-    @staticmethod
-    def do(player):
-        if player.check_cannon == False:
-            player.frame = (player.frame + 1) % 3
-        else:
-            player.frame = (player.frame + 1) % 24
-            if player.frame >= 23:
-                player.check_cannon = False
-
-
-    @staticmethod
-    def draw(player):
-        if player.check_cannon == False:
-            player.image.clip_draw(player.frame * 80, 160, 80 - 3, 80, player.x, player.y)
-        else:
-            player.image.clip_draw(player.frame * 140, 0, 140, 80, player.x + 40, player.y)
-
-
-class DriveState:
-
-    @staticmethod
-    def enter(player, event):
-        if event == RIGHT_DOWN:
-            player.velocity += 1
-        elif event == LEFT_DOWN:
-            player.velocity -= 1
-        elif event == RIGHT_UP:
-            player.velocity -= 1
-        elif event == LEFT_UP:
-            player.velocity += 1
-
-    @staticmethod
-    def exit(player, event):  # 왜 나가는지 event를 통해서 알려줄 수 있음.
-        if event == X:
+            player.frame = 0
             player.check_cannon = True
             player.fire_cannon()
 
     @staticmethod
     def do(player):
         if player.check_cannon:
-            player.frame = (player.frame + 1) % 23
-        else:
-            player.frame = (player.frame + 1) % 18
-            if player.frame >= 23:
+            player.frame = (player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 23
+            if player.frame >= 22:
                 player.check_cannon = False
-        player.x += player.velocity
+        else:
+            player.frame = (player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
+
+
+
+    @staticmethod
+    def draw(player):
+        if player.check_cannon:
+            player.image.clip_draw(int(player.frame) * 140, 0, 140, 80, player.x + 40, player.y)
+        else:
+            player.image.clip_draw(int(player.frame) * 80, 160, 80 - 3, 80, player.x, player.y)
+
+class DriveState:
+
+    @staticmethod
+    def enter(player, event):
+        if event == RIGHT_DOWN:
+            player.velocity += RUN_SPEED_PPS
+        elif event == LEFT_DOWN:
+            player.velocity -= RUN_SPEED_PPS
+        elif event == RIGHT_UP:
+            player.velocity -= RUN_SPEED_PPS
+        elif event == LEFT_UP:
+            player.velocity += RUN_SPEED_PPS
+        player.dir = clamp(-1, player.velocity, 1)
+
+    @staticmethod
+    def exit(player, event):  # 왜 나가는지 event를 통해서 알려줄 수 있음.
+        if event == X:
+            player.frame = 0
+            player.check_cannon = True
+            player.fire_cannon()
+
+    @staticmethod
+    def do(player):
+        if player.check_cannon:
+            player.frame = (player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 23
+            if player.frame >= 22:
+                player.check_cannon = False
+        else:
+            player.frame = (player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 18
+
+        player.x += player.velocity * game_framework.frame_time
         player.x = clamp(40, player.x, 1600 - 40)
 
     @staticmethod
     def draw(player):
-        if player.check_cannon == False:
-            player.image.clip_draw(player.frame * 80, 80, 80-3, 80, player.x, player.y)
+        if player.check_cannon:
+            player.image.clip_draw(int(player.frame) * 140, 0, 140, 80, player.x + 40, player.y)
         else:
-            player.image.clip_draw(player.frame * 140, 0, 140, 80, player.x, player.y)
+            player.image.clip_draw(int(player.frame) * 80, 80, 80 - 3, 80, player.x, player.y)
 
 
 next_state_table = {
@@ -118,42 +123,6 @@ next_state_table = {
                }
 }
 
-
-class Boy:
-
-    def __init__(self):
-        self.x, self.y = 1600 // 2, 90
-        self.image = load_image('finaltest.png')
-        self.dir = 1
-        self.velocity = 0
-        self.frame = 0
-        self.event_que = []
-        self.cur_state = IdleState
-        self.cur_state.enter(self, None)
-
-    def fire_ball(self):
-        ball = Cannon(self.x, self.y, self.dir*3)
-        game_world.add_object(ball, 1)
-
-    def add_event(self, event):
-        self.event_que.insert(0, event)
-
-    def update(self):
-        self.cur_state.do(self)
-        if len(self.event_que) > 0:
-            event = self.event_que.pop()
-            self.cur_state.exit(self, event)
-            self.cur_state = next_state_table[self.cur_state][event]
-            self.cur_state.enter(self, event)
-
-    def draw(self):
-        self.cur_state.draw(self)
-
-
-    def handle_event(self, event):
-        if (event.type, event.key) in key_event_table:
-            key_event = key_event_table[(event.type, event.key)]
-            self.add_event(key_event)
 
 class Player:
     def __init__(self):
