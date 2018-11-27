25b6c0049563da624ceb9b765ab9febf49c619f9
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-27 18:50:26 +0900

    Drill #15 ranking_state
    틀 구현

--- Drills/Drill-15/ranking_state.py
+++ Drills/Drill-15/ranking_state.py
@@ -1,1 +1,32 @@
-pass
+import main_state
+import world_build_state
+import json
+
+__name__ = 'ranking_state'
+
+def enter():
+    pass
+
+
+def exit():
+   pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    pass
